7f34eae99db4072583ce1e2dad631b648625b1e0
Mubson123 <cedricmube@gmail.com>
2025-10-28 18:30:05 +0100

    update test environment done

--- registration-service/src/test/java/com/mc/citizen/controller/CitizenControllerIT.java
+++ registration-service/src/test/java/com/mc/citizen/controller/CitizenControllerIT.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
@@ -18,8 +19,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CitizenControllerIT {
     static final String BASE_PATH = "/api/v2/registration/citizens";
