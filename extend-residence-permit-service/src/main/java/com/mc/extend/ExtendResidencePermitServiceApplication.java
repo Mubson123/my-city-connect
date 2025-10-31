db7a883bb322134bc5eb29b3dba4fdc94851614d
Mubson123 <cedricmube@gmail.com>
2025-10-31 14:03:37 +0100

    add new service to extend a residence permit done

---
+++ extend-residence-permit-service/src/main/java/com/mc/extend/ExtendResidencePermitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mc.extend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendResidencePermitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendResidencePermitServiceApplication.class, args);
+	}
+
+}
